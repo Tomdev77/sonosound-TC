1e33c7fe420d88d0efa49698b7176bddb56812f8
Thom_i <62951436+Tomdev77@users.noreply.github.com>
2024-03-07 09:40:58 +0100

    Update programmations.jsx

--- src/components/pages/programmations.jsx
+++ src/components/pages/programmations.jsx
@@ -27,7 +27,7 @@ const Artistes = () => {
             const basicAuth = btoa(`${username}:${password}`);
             
             // Définition de l'URL de l'API à interroger
-             const apiUrl = 'https://sonosound.online/wp-json/wp/v2/posts?per_page=150';
+             const apiUrl = 'https://sonosound.online/wp-json/wp/v2/posts?per_page=100';
 
             
             // Définition de l'URL du proxy pour contourner les restrictions CORS
