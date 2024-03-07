ae311e3f7bc89df4293662d55bc9844a1f3db3eb
Thom_i <62951436+Tomdev77@users.noreply.github.com>
2024-03-07 09:32:09 +0100

    Update map.jsx

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,59 +1,52 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'; // importation carte  de la bibliothèque js leaflet
-import { divIcon, point, Icon } from 'leaflet'; // importation emoticone de leaflet
-import 'leaflet/dist/leaflet.css'; // importation css leaflet
-import { FullscreenControl } from 'react-leaflet-fullscreen'; // importation ullscreen sur carte leaflet
-import MarkerClusterGroup from 'react-leaflet-cluster'; // importation cluster (infos) sur carte leaflet
-import '../css/map.css'; // importation carte fichier css
-import shopIcon from "../assets/shop.png"; // importation emoticone complémentaire
-import WCIcon from "../assets/wc.png"; // importation emoticone complémentaire
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import { divIcon, point, Icon } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { FullscreenControl } from 'react-leaflet-fullscreen';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import axios from 'axios';
+import WCIcon from "../assets/wc.png";
+import BuvetteIcon from "../assets/bar.png"; // import de l'icône pour les buvettes
+import SceneIcon from "../assets/scene.png"; // import de l'icône pour les scènes
+import "../../src/css/map.css"; // * importation du fichier css concerts, style
+
+const WCMarkerIcon = new Icon({
+  iconUrl: WCIcon,
+  iconSize: [38, 38],//  l'icône pour les wc
+});
+
+const BuvetteMarkerIcon = new Icon({ //  l'icône pour les buvettes
+  iconUrl: BuvetteIcon,
+  iconSize: [38, 38],
+});
+
+const SceneMarkerIcon = new Icon({ //  l'icône pour les scènes
+  iconUrl: SceneIcon,
+  iconSize: [38, 38],
+});
 
 
-const customIcon = new Icon({
-    iconUrl: require("../assets/location-pin.png"), // récupération de l'image dans les assets
-    iconSize: [38, 38] // définition de sa taille
-  });
-
-  const shopMarkerIcon = new Icon({
-    iconUrl: shopIcon, // récupération de l'image dans les assets
-    iconSize: [38, 38], // définition de sa taille
-  });
-
-  const WCMarkerIcon = new Icon({
-    iconUrl: WCIcon, // récupération de l'image dans les assets
-    iconSize: [38, 38], // définition de sa taille
-  });
-
-
-  
-const createClusterCustomIcon = function (cluster) { // fonction création cluster emoticones // groupe de marqueurs =>> cartographie
-  return new divIcon({
-    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
-    className: 'custom-marker-cluster',
-    iconSize: point(33, 33, true), // 
-  });
-};
-
-const markers = [ // tableau markers
+const sceneMarkers = [ // tableau markers
   {
     geocode: [48.9000, 2.1000], // géocode latitude + longitude 
-    popUp: 'Scène 4 - 1 Pl. Charles de Gaulle, 78100 Saint-Germain-en-Laye', // pop up figurant sur la carte
+    id : 794,
   },
   {
     geocode: [48.9113, 2.0869],
-    popUp: 'Scène 5 - Golf Saint germain -  Route de Poissy - 78100 Saint-Germain-en-Laye',
+    id : 796,
   },
   {
     geocode: [48.8606, 2.2353],
-    popUp: 'Scène 3 - Bois de Boulogne -  75016 Paris',
+    id: 792,
   },
+  
   {
     geocode: [48.8465, 2.2204],
-    popUp: 'Scène 2 - Parc de Saint-Cloud - 92210 Saint-Cloud France',
+    id: 790,
   },
   {
     geocode: [48.8566, 2.3522],
-    popUp: 'Scène 1 (principal) - Hôtel de Ville 5 Rue de Lobau - 75004 Paris',
+    id:788,
   },
 
 ];
@@ -61,76 +54,116 @@ const markers = [ // tableau markers
 const markershop= [
   {
     geocode: [48.9000, 2.1000],
-    popUp: 'Buvette Scène 4 - 1 Pl. Charles de Gaulle, 78100 Saint-Germain-en-Laye',
+    id: 804,
   },
   {
     geocode: [48.9113, 2.0869],
-    popUp: 'Buvette Scène 5 - Golf Saint germain -  Route de Poissy - 78100 Saint-Germain-en-Laye',
+    id: 806,
   },
   {
     geocode: [48.8606, 2.2353],
-    popUp: ' Buvette Scène 3 - Bois de Boulogne -  75016 Paris',
-  },
+    id: 802,  },
   {
     geocode: [48.8465, 2.2204],
-    popUp: ' Buvette Scène 2 - Parc de Saint-Cloud - 92210 Saint-Cloud France',
-  },
+    id: 800,   },
   {
     geocode: [48.8566, 2.3522],
-    popUp: 'Buvette Scène 1 (principal) - Hôtel de Ville 5 Rue de Lobau - 75004 Paris',
-  },
+    id: 798,  },
 
 ];
 
 const markerwc= [
   {
     geocode: [48.9000, 2.1000],
-    popUp: 'WC Scène 4 - 1 Pl. Charles de Gaulle, 78100 Saint-Germain-en-Laye',
+    id: 814,
   },
   {
     geocode: [48.9113, 2.0869],
-    popUp: 'WC Scène 5 - Golf Saint germain -  Route de Poissy - 78100 Saint-Germain-en-Laye',
+    id: 796,
   },
   {
     geocode: [48.8606, 2.2353],
-    popUp: ' WC Scène 3 - Bois de Boulogne -  75016 Paris',
+    id: 812,
   },
   {
     geocode: [48.8465, 2.2204],
-    popUp: ' WC Scène 2 - Parc de Saint-Cloud - 92210 Saint-Cloud France',
+    id: 810,
   },
   {
     geocode: [48.8566, 2.3522],
-    popUp: 'WC Scène 1 (principal) - Hôtel de Ville 5 Rue de Lobau - 75004 Paris',
+    id: 808,
   },
 
 ];
 
-
 const CustomMap = () => {
-  const [userLocation, setUserLocation] = useState(null); // État pour la localisation de l'utilisateur
-  const mapRef = useRef(null); // Référence à l'instance de la carte
-  
+  const [userLocation, setUserLocation] = useState(null);
+  const mapRef = useRef(null);
+  const [markerWC, setMarkerWC] = useState([]);
+  const [markerBuvettes, setMarkerBuvettes] = useState([]);
+  const [markerScenes, setMarkerScenes] = useState([]);
+
+  const username = 'tom';
+  const password = 'Petitcalvejunior2025!!$$';
+  const apiUrl = 'https://sonosound.online/wp-json/wp/v2/posts?per_page=100';
+  const proxyUrl = '/proxy?url=';
+
   useEffect(() => {
-    if (userLocation) {
-      mapRef.current.setView(userLocation, 15); // Zoom to user location with zoom level 15
-    }
-  }, [userLocation]);
+    const fetchData = async () => {
+      try {
+        const basicAuth = btoa(`${username}:${password}`);
+        const response = await axios.get(`${proxyUrl}${encodeURIComponent(apiUrl)}`, {
+          headers: {
+            Authorization: `Basic ${basicAuth}`
+          }
+        });
+        const data = response.data;
 
-  const locateUser = () => {
-    mapRef.current.locate({setView: true}); // vue location trouvé
-  };
+        const filteredWC = data.filter(article => [814, 796, 812, 810, 808].includes(article.id));
+        const filteredBuvettes = data.filter(article => [804, 806, 802, 800, 798].includes(article.id));
+        const filteredScenes = data.filter(article => [796, 794, 792, 790, 788].includes(article.id));
 
-  const [markerUser, setMarkerUser] = useState([]);
+        const wcMarkers = filteredWC.map(wc => ({
+          geocode: [wc.latitude, wc.longitude],
+          popUp: wc.nom,
+        }));
 
+        const buvetteMarkers = filteredBuvettes.map(buvette => ({
+          geocode: [buvette.latitude, buvette.longitude],
+          popUp: buvette.nom,
+        }));
+
+        const sceneMarkers = filteredScenes.map(scene => ({
+          geocode: [scene.latitude, scene.longitude],
+          popUp: scene.nom,
+        }));
+
+        setMarkerWC(wcMarkers);
+        setMarkerBuvettes(buvetteMarkers);
+        setMarkerScenes(sceneMarkers);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleLocationFound = (e) => {
     setUserLocation(e.latlng);
-  
-    // Mettre à jour markerUser avec la position de l'utilisateur
-    setMarkerUser([{ geocode: [e.latlng.lat, e.latlng.lng], popUp: 'Ma position' }]);
   };
 
+  const locateUser = () => {
+    mapRef.current.locate({setView: true});
+  };
+
+  const createClusterCustomIcon = function (cluster) {
+    return new divIcon({
+      html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+      className: 'custom-marker-cluster',
+      iconSize: point(33, 33, true),
+    });
+  };
 
   return (
     <div className="map-container">
@@ -142,46 +175,41 @@ const CustomMap = () => {
         ref={mapRef}
         onLocationfound={handleLocationFound}
       >
-       <TileLayer
-    attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.mapbox.com">MapBox</a>'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-/>
-{userLocation && <Marker position={userLocation}><Popup>Votre position actuelle</Popup></Marker>} {/* Marqueur utilisateur */}
+        <TileLayer
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.mapbox.com">MapBox</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {userLocation && <Marker position={userLocation}><Popup>Votre position actuelle</Popup></Marker>}
         <MarkerClusterGroup
           chunkedLoading
           iconCreateFunction={createClusterCustomIcon}
         >
-          {markers.map((marker, index) => ( /* boucle parmi les marqueurs*/
-            <Marker key={index} position={marker.geocode} icon={customIcon}>
-              <Popup>{marker.popUp}</Popup> {/* Popup pour le  marqueur */}
-            </Marker>
-          ))}
-
-          {markershop.map((marker, index) => (
-            <Marker key={index} position={marker.geocode} icon={shopMarkerIcon}>
-              <Popup>{marker.popUp}</Popup>
-            </Marker>
-          ))}
-
-          {markerwc.map((marker, index) => (
+          {markerWC.map((marker, index) => (
             <Marker key={index} position={marker.geocode} icon={WCMarkerIcon}>
               <Popup>{marker.popUp}</Popup>
             </Marker>
           ))}
 
-   
+          {markerBuvettes.map((marker, index) => (
+            <Marker key={index} position={marker.geocode} icon={BuvetteMarkerIcon}>
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+
+          {markerScenes.map((marker, index) => (
+            <Marker key={index} position={marker.geocode} icon={SceneMarkerIcon}>
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+
         </MarkerClusterGroup>
         <FullscreenControl position="topright" aria-hidden="true">  </FullscreenControl>
-
-    <button className="locationbuttonMap" onClick={locateUser}>
-      Se géolocaliser
-    </button>
+        <button className="locationbuttonMap" onClick={locateUser}>
+          Se géolocaliser
+        </button>
       </MapContainer>
-      {/* La fonction est appelée et met à jour la référence à l'instance de la carte */}
-
     </div>
   );
 };
 
 export default CustomMap;
-
